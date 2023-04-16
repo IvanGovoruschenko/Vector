9a271ce44cc264e1c9463423b5bcb51211ddc7df
IvanGovoruschenko <ivan.lenovo.govor@gmail.com>
2023-04-16 19:47:05 +0300

    "append push / pop functions"

--- main.c
+++ main.c
@@ -1,21 +1,53 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <assert.h>
 
 #include "vector.h"
 
-int main()
+void test_pushBack_emptyVector()
 {
     Vector v = createVector(0);
 
-    printf("%d ", v.data[0]);
-    printf("%d ", v.data[1]);
-    printf("%d ", v.data[2]);
+    pushBack(&v, 228);
+    assert(v.size == 1 && v.capacity == 1 && getVectorValue(&v, 0) == 228);
+}
 
-    reserve(&v, 2);
+void test_pushBack_fullVector()
+{
+    Vector v = createVector(1);
+    *v.data = 1337;
+    v.size = 1;
 
-    printf("%d ", v.data[0]);
-    printf("%d ", v.data[1]);
-    printf("%d ", v.data[2]);
+    pushBack(&v, 228);
+    assert(v.size == 2 && getVectorValue(&v, 0) == 1337 && getVectorValue(&v, 1) == 228);
+
+    pushBack(&v, 1488);
+    assert(v.size == 3 && v.capacity == 4 && getVectorValue(&v, 0) == 1337 &&
+           getVectorValue(&v, 1) == 228 && getVectorValue(&v, 2) == 1488);
+}
+
+void test_popBack_notEmptyVector()
+{
+    Vector v = createVector(0);
+
+    pushBack(&v, 10);
+    assert(v.size == 1);
+
+    popBack(&v);
+    assert(v.size == 0);
+    assert(v.capacity == 1);
+}
+
+void test()
+{
+    test_pushBack_emptyVector();
+    test_pushBack_fullVector();
+    test_popBack_notEmptyVector();
+}
+
+int main()
+{
+    test();
 
     return 0;
 }
--- vector.h
+++ vector.h
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stdbool.h>
 
 #ifndef VECTOR_VECTOR_H
 #define VECTOR_VECTOR_H
@@ -15,16 +15,32 @@ typedef struct Vector
 // возвращает структуру-дескриптор вектор из n значений
 Vector createVector(size_t n);
 
-// изменяет количество памяти, выделенное под хранение элементов вектора.
+// изменяет количество памяти, выделенное под хранение элементов вектора v на newCapacity
 void reserve(Vector *v, size_t newCapacity);
 
-//
+// удаляет элементы из вектора v, но не освобождает выделенную память
 void clear(Vector *v);
 
-//
+// освобождает память, выделенную под неиспользуемые элементы вектора v
 void shrinkToFit(Vector *v);
 
-//
+// освобождает память, выделенную вектору v
 void deleteVector(Vector *v);
+
+// возвращает "истина", если вектор v пустой, иначе - "ложь"
+bool isEmpty(Vector *v);
+
+// возвращает "истина", если вектор v полный, иначе - "ложь"
+bool isFull(Vector *v);
+
+// возвращает i-ый элемент вектора v
+int getVectorValue(Vector *v, size_t i);
+
+// добавляет элемент x в конец вектора v. Если вектор заполнен, увеличивает количество выделенной ему памяти в
+// 2 раза, используя reserve
+void pushBack(Vector *v, int x);
+
+// удаляет последний элемент из вектора v
+void popBack(Vector *v);
 
 #endif //VECTOR_VECTOR_H
